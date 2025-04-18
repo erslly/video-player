7b8445acd535abc33b385408ec5b03dfc6b0e31e
Erdem <157350349+erslly@users.noreply.github.com>
2025-04-18 21:46:00 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <VideoPlayer
           src="https://p.erslly.xyz/%5BSeicode%5D%20The%20Too-Perfect%20Saint%20Tossed%20Aside%20by%20My%20Fianc%C3%A9%20and%20Sold%20to%20Another%20Kingdom%20-%2001%20%5B1080p%5D%20(1)%20(1).mp4"
           poster="/placeholder.svg?height=720&width=1280"
-          title="Big Buck Bunny"
+          title="The Too-Perfect Saint Tossed Aside by My Fiancé and Sold to Another Kingdom"
         />
       </div>
     </main>
